a3c7dd2a794426aab383de778596e9cfbe96bcbc
Alexandr <san.az.a.h.a.r.o.v345@gmail.com>
2022-09-02 16:16:44 +0300

    Start making merge board, pre setup animals, import astar

---
+++ animals project/Animals/Assets/Code/Logic/Boards/MergeBoard.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Logic.Boards
+{
+    public class MergeBoard : MonoBehaviour
+    {
+        [SerializeField] private Tile _tilePrefab;
+        [SerializeField] private Vector2Int _boardSize;
+
+        private Tile[] _tiles;
+
+        public IReadOnlyCollection<Tile> Tiles => _tiles;
+
+
+        private void Start()
+        {
+            SpawnTiles();
+        }
+
+        private void SpawnTiles()
+        {
+            for (int i = 0; i < _boardSize.x; i++)
+            {
+                for (int j = 0; j < _boardSize.y; j++)
+                {
+                    Tile tile = Instantiate(_tilePrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+                    tile.Position = new Vector2Int(i, j);
+                    
+                }
+            }
+        }
+    }
+}
---
+++ animals project/Animals/Assets/Code/Logic/Boards/Tile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Code.Logic.Boards
+{
+    public class Tile : MonoBehaviour
+    {
+        public Vector2Int Position { get; set; }
+    }
+}
